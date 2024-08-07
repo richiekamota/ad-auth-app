22d0eb7f088f7545a4b8081921ba51b27d88ca50
richiekamota <richiekamota@gmail.com>
2024-08-07 17:43:02 +0200

    adjustments in the User models

--- application/app/Ldap/User.php
+++ application/app/Ldap/User.php
@@ -5,8 +5,9 @@ namespace App\Ldap;
 use LdapRecord\Models\Model;
 use LdapRecord\Models\Concerns\CanAuthenticate;
 use Illuminate\Contracts\Auth\Authenticatable;
+use LdapRecord\Laravel\Auth\LdapAuthenticatable;
 
-class User extends Model implements Authenticatable
+class User extends Model implements Authenticatable,  LdapAuthenticatable
 {
   use CanAuthenticate;
 
@@ -16,6 +17,39 @@ class User extends Model implements Authenticatable
 
   public function getAuthIdentifier()
   {
-    // TODO: Implement getAuthIdentifier() method.
+    return $this->guidKey;
+  }
+
+  public function getLdapDomainColumn(): string
+  {
+    return 'ldap_host';
+  }
+
+  public function getLdapDomain(): ?string
+  {
+    return $this->ldap_host;
+  }
+
+  public function setLdapDomain(?string $domain): void
+  {
+    $this->ldap_host = $domain;
+    $this->save();
+  }
+
+  public function getLdapGuidColumn(): string
+  {
+    return 'guid';
+  }
+
+  public function getLdapGuid(): ?string
+  {
+    return $this->ldap_guid;
+  }
+
+  public function setLdapGuid(?string $guid): void
+  {
+    $this->ldap_guid= $guid;
+    $this->save();
   }
 }
+
