6416acbb42fd4f3e5ca9d40a0ee9d1e7aeb4d724
richiekamota <richiekamota@gmail.com>
2024-08-08 20:07:55 +0200

    corrected the config/auth.php file settings

--- application/config/ldap.php
+++ application/config/ldap.php
@@ -29,7 +29,7 @@ return [
     'connections' => [
 
         'default' => [
-            'hosts' => [env('LDAP_HOST', 'localhost')],
+            'hosts' => [env('LDAP_HOST', '127.0.0.1')],
             'username' => env('LDAP_USERNAME', 'cn=LDIF,dc=localhost,dc=com'),
             'password' => env('LDAP_PASSWORD', 'kamota'),
             'port' => env('LDAP_PORT', 9011),
